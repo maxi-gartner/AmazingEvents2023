72909a56ab5b6528e49cb98c4d75956c7c8c0cb3
Maximiliano Gartner <maxi-gartner@hotmail.com>
2023-04-12 14:51:17 -0300

    :sparkles: add fetch and then all pages

--- assets/data/app_past.js
+++ assets/data/app_past.js
@@ -1,26 +1,36 @@
-import {data} from './module/data.js'
 import {printCard, categoriesFilter, categoriesPrint, mixCheck, categoriesActiveMediaQuery, captureCheckboxCheked, filterCategoriesChecked, filterSearch, carrucelSearch, colorChecked, filterEvents, $} from './module/functions.js'
 
-const eventsFilter = data.eventos
-const currentDate = data.fechaActual
+
 const events= []
 const categories = []
 const categoriesChecked = [];
 const searchEventCheked = [];
 const address = "./"
 
+const url = 'https://mindhub-xj03.onrender.com/api/amazing'
+    fetch(url)
+    .then((res)=> res.json())
+    .then((data)=> {
+        console.log(data)
+        const eventsFilter = data.events
+        const currentDate = data.currentDate
+
+        filterEvents(eventsFilter, events, currentDate , false)
+        printCard(events, conteinerCards, address);
+        categoriesFilter(events, categories);
+        categoriesPrint(categories, categoriesContainer);
+        categoriesActiveMediaQuery(categories, categoriesContainer)
+        mixCheck()
+        captureCheckboxCheked(categoriesChecked, conteinerCards);
+        filterCategoriesChecked(events, categoriesChecked, conteinerCards, searchEventCheked, eventsFilter, address)
+        filterSearch(events, eventsFilter, categoriesChecked, searchEventCheked, conteinerCards, address)
+        carrucelSearch(search)
+        colorChecked()
+        console.log(events.length)
+    }) 
+    .catch((error)=> console.log("error", error))
+
+
 const conteinerCards = $("cards");
 const categoriesContainer = $("categories");
 const search = $("search");
-
-filterEvents(eventsFilter, events, currentDate, false)
-printCard(events, conteinerCards, address);
-categoriesFilter(events, categories);
-categoriesPrint(categories, categoriesContainer);
-categoriesActiveMediaQuery(categories, categoriesContainer)
-mixCheck()
-captureCheckboxCheked(categoriesChecked, conteinerCards);
-filterCategoriesChecked(events, categoriesChecked, conteinerCards, searchEventCheked, eventsFilter, address)
-filterSearch(events, eventsFilter, categoriesChecked, searchEventCheked ,conteinerCards, address)
-carrucelSearch(search)
-colorChecked(categoriesContainer)
